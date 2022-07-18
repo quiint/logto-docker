ff627cef111831f4a7834492ab5ecb182e252383
Starry North <gonorth@qq.com>
2022-07-18 22:26:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,25 @@ const directory = 'logto';
 const nodeMajorVersion = 16;
 
 (async () => {
-  if (existsSync(directory)) {
-    throw new Error(`\`${directory}\` already exists in the current directory.`);
+  if (!existsSync(directory)) {
+    const nodeVersion = execSync('node -v', { encoding: 'utf-8' });
+
+    if (!isVersionGreaterThan(trimV(nodeVersion), nodeMajorVersion)) {
+      throw new Error(`Logto requires NodeJS >= ${nodeMajorVersion}.0.0.`);
+    }
+
+    // Download and extract
+    spawnSync(
+      'sh',
+      ['-c', 'curl -L https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz | tar -xz'],
+      { stdio: 'inherit' },
+    );
+
+    // Rebuild Argon2
+    spawnSync(
+      'sh',
+      ['-c', 'npx node-pre-gyp rebuild -C .'],
+      { stdio: 'inherit', cwd: './logto/packages/core/node_modules/argon2' },
+    );
   }
-
-  const nodeVersion = execSync('node -v', { encoding: 'utf-8' });
-
-  if (!isVersionGreaterThan(trimV(nodeVersion), nodeMajorVersion)) {
-    throw new Error(`Logto requires NodeJS >= ${nodeMajorVersion}.0.0.`);
-  }
-
-  // Download and extract
-  spawnSync(
-    'sh',
-    ['-c', 'curl -L https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz | tar -xz'],
-    { stdio: 'inherit' },
-  );
-
-  // Rebuild Argon2
-  spawnSync(
-    'sh',
-    ['-c', 'npx node-pre-gyp rebuild -C .'],
-    { stdio: 'inherit', cwd: './logto/packages/core/node_modules/argon2' },
-  );
 })();
