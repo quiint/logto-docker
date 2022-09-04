e7006c5017234d0781050c26bcbda46b385d2b3d
sovlookup <gonorth@qq.com>
2022-09-04 14:09:12 +0800

    更新

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-const { execSync, spawnSync, spawn } = require('child_process');
-const { existsSync } = require('fs');
+const { spawnSync, spawn } = require('child_process')
+const { existsSync } = require('fs')
 
-const isVersionGreaterThan = (version, targetMajor) => Number(version.split('.')[0]) >= targetMajor;
+const directory = 'logto'
+const startCommand = `cd ${directory} && npm start`
 
-const trimV = (version) => version.startsWith('v') ? version.slice(1) : version;
-
-const directory = 'logto';
-const nodeMajorVersion = 16;
-const startCommand = `cd ${directory} && npm start`;
-const main = async () => {
+try {
   if (!existsSync(directory)) {
-    const nodeVersion = execSync('node -v', { encoding: 'utf-8' });
-
-    if (!isVersionGreaterThan(trimV(nodeVersion), nodeMajorVersion)) {
-      throw new Error(`Logto requires NodeJS >= ${nodeMajorVersion}.0.0.`);
-    }
-
     // Download and extract
     spawnSync(
       'sh',
-      ['-c', 'curl -L https://tool.mintimate.cn/ghs/https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz | tar -xz'],
+      [
+        '-c',
+        'curl -L https://tool.mintimate.cn/ghs/https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz | tar -xz',
+      ],
       { stdio: 'inherit' },
-    );
-
-    // Rebuild Argon2
-    spawnSync(
-      'sh',
-      ['-c', 'npx node-pre-gyp rebuild -C .'],
-      { stdio: 'inherit', cwd: './logto/packages/core/node_modules/argon2' },
-    );
+    )
   }
-  spawn('sh', ['-c', startCommand], { stdio: 'inherit' });
+} finally {
+  spawn('sh', ['-c', startCommand], { stdio: 'inherit' })
 }
-
-main()
