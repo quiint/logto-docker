6cce14a0c5ff3ded44adb5263adf2d428a5d0ef9
sovlookup <gonorth@qq.com>
2022-09-04 14:35:27 +0800

    更新

--- build.js
+++ build.js
@@ -5,7 +5,8 @@ spawnSync(
   'sh',
   [
     '-c',
-    'curl -L https://tool.mintimate.cn/ghs/https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz | tar -xz',
+    'curl -L https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz | tar -xz',
+    // 'curl -L https://tool.mintimate.cn/ghs/https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz | tar -xz',
   ],
   { stdio: 'inherit' },
 )
