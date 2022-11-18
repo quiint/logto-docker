c499fd2b9491af1e763348037c70cad25a7fda93
fan xia <gonorth@qq.com>
2022-11-18 20:27:57 +0800

    ok

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
-    logto init -p /app
+    logto init -p /app -du /tmp/logto.tar.gz
 else
     cd /app && npm start
 fi
