d31b63bf099c6339361b0a0d65789f3d35a0ae38
Quiint <quiint@quiint.email>
2022-12-16 09:51:33 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     logto init -p /app --download-url /tmp/logto.tar.gz $ARGS --skip-seed
 else
-    cd /app
-    npx @logto/cli connector add --official
+    cd /app && npx @logto/cli connector add --official
     npm start
 fi
