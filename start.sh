e499117992e70e374253036d81dd61c7a8728136
Quiint <quiint.gh@gmail.com>
2022-12-16 09:38:25 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
-    logto init -p /app --download-url /tmp/logto.tar.gz $ARGS
+    logto init -p /app --download-url /tmp/logto.tar.gz $ARGS --skip-seed
 else
     cd /app && npm start
 fi
